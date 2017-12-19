4dcee5aff06aed9dd91b0f68df748ad6f72f8fae
Alex Corbi <a.corbi@gmail.com>
2017-12-19 19:44:51 +0100

    Fixes #93

--- ckanext/odm_nav/lib/odm_nav_helper.py
+++ ckanext/odm_nav/lib/odm_nav_helper.py
@@ -183,7 +183,7 @@ def current_url_with_locale(new_lang):
   current_url = request.environ['CKAN_CURRENT_URL']
   current_lang_path = "/" + request.environ['CKAN_LANG']
   new_lang_path = "/" + new_lang
-  new_path = current_url.replace(current_lang_path,new_lang_path)
+  new_path = new_lang_path + current_url
   return urllib.unquote(new_path)
 
 session = {}
