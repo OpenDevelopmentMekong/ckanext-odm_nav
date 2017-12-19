13801ad4eab24955a9625dffb69df890158d958f
Alex Corbi <a.corbi@gmail.com>
2017-12-19 19:42:07 +0100

    Fixes #93

--- ckanext/odm_nav/lib/odm_nav_helper.py
+++ ckanext/odm_nav/lib/odm_nav_helper.py
@@ -181,8 +181,8 @@ def sanitize_html(string):
 
 def current_url_with_locale(new_lang):
   current_url = request.environ['CKAN_CURRENT_URL']
-  current_lang_path = "/" + request.environ['CKAN_LANG'] + "/"
-  new_lang_path = "/" + new_lang + "/"
+  current_lang_path = "/" + request.environ['CKAN_LANG']
+  new_lang_path = "/" + new_lang
   new_path = current_url.replace(current_lang_path,new_lang_path)
   return urllib.unquote(new_path)
 
