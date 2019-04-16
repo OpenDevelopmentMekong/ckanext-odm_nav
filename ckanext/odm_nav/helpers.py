67c365f0b8523490ff4ae1d9182fd895e44f5028
Eric Soroos <eric@derilinx.net>
2019-04-16 09:44:37 +0000

    return blank on invalid resource, add large parameter

--- ckanext/odm_nav/helpers.py
+++ ckanext/odm_nav/helpers.py
@@ -368,9 +368,12 @@ def odm_nav_menu(site=None):
         
     return menus.rendered[site]
 
-def odm_wms_download(resource):
-    ows_server = resource['wms_server'].replace('/wms', '/')
-    layer = resource['wms_layer']
+def odm_wms_download(resource, large=True):
+    try:
+        ows_server = resource['wms_server'].replace('/wms', '/')
+        layer = resource['wms_layer']
+    except:
+        return ''
     namespace = ''
 
     try:
@@ -394,13 +397,14 @@ def odm_wms_download(resource):
     dl_list = "\n".join([ link_templ % (_url(fmt), name) for name, fmt in output_formats])
 
     return """<span class='dropdown'>
-	   <a class='btn btn-primary btn-download btn-block btn-lg' id="a_wms_dl_%s" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">%s
+	   <a class='btn btn-primary btn-download %s' id="a_wms_dl_%s" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">%s
 	     <span class="caret"></span>
 	     </a>
 	   <div class='dropdown-menu' aria-labelledby="a_wms_dl_%s">
              %s
 	   </div>
-	 </span>""" % (resource['id'],
+	 </span>""" % (large and "btn-block btn-lg" or "",
+                       resource['id'],
                        _('Download'),
                        resource['id'],
                        dl_list)
