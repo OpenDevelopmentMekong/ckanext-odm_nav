bba006061d6b0a232a529cd82abb8595cca36bb4
Eric Soroos <eric@derilinx.com>
2019-11-05 17:03:43 +0000

    Use more modern, async loading.

--- ckanext/odm_nav/fanstatic/megamenu.js
+++ ckanext/odm_nav/fanstatic/megamenu.js
@@ -1,3 +1,9 @@
-document.write("<script type='text/javascript' src='"+megamenu.wp_url+"/wp-includes/js/hoverIntent.min.js?ver=1.8.1'></script>"+
-  "<script type='text/javascript' src='"+megamenu.wp_url+"/wp-content/plugins/megamenu/js/maxmegamenu.js?ver=2.6'></script>"+
-  "<script type='text/javascript' src='"+megamenu.wp_url+"/wp-content/plugins/megamenu-pro/assets/public.js?ver=1.6.2.1'></script>");
+$.ajaxSetup({
+  cache: true
+});
+$.getScript(megamenu.wp_url+"/wp-includes/js/hoverIntent.min.js?ver=1.8.1");
+$.getScript(megamenu.wp_url+"/wp-content/plugins/megamenu/js/maxmegamenu.js?ver=2.6");
+$.getScript(megamenu.wp_url+"/wp-content/plugins/megamenu-pro/assets/public.js?ver=1.6.2.1");
+$.ajaxSetup({
+  cache: false
+});
