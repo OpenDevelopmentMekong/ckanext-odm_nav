2557b36e7aed3be586c97683da7f1164b8b2a094
gtkChop <swaroopbhatk@gmail.com>
2021-09-11 16:51:33 +0000

    wp url megamenu #769

--- ckanext/odm_nav/fanstatic/megamenu.js
+++ ckanext/odm_nav/fanstatic/megamenu.js
@@ -1,9 +1,9 @@
 $.ajaxSetup({
   cache: true
 });
-$.getScript(megamenu.wp_url+"/wp-includes/js/hoverIntent.min.js?ver=1.8.1");
-$.getScript(megamenu.wp_url+"/wp-content/plugins/megamenu/js/maxmegamenu.js?ver=2.6");
-$.getScript(megamenu.wp_url+"/wp-content/plugins/megamenu-pro/assets/public.js?ver=1.6.2.1");
+$.getScript("https://opendevelopmentmekong.net/wp-includes/js/hoverIntent.min.js?ver=1.8.1");
+$.getScript("https://opendevelopmentmekong.net/wp-content/plugins/megamenu/js/maxmegamenu.js?ver=2.6");
+$.getScript("https://opendevelopmentmekong.net/wp-content/plugins/megamenu-pro/assets/public.js?ver=1.6.2.1");
 $.ajaxSetup({
   cache: false
 });
